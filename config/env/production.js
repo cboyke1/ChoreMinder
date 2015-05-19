5f5e6883d61e2440500855f2ea6861c8df32ef48
cboyke1 <cboyke@gmail.com>
2015-05-19 13:39:18 -0700

    CSS changes

--- config/env/production.js
+++ config/env/production.js
@@ -10,6 +10,8 @@ module.exports = {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.min.css',
 				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+				'public/modules/core/css/core.css',
+				'public/modules/users/css/users.css'
 			],
 			js: [
 				'public/lib/angular/angular.min.js',
